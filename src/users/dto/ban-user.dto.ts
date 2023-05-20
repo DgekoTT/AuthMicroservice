f7c26d9b308b71ab702cefca60975dbf4a67d1cc
Dgeko <dfhdfh13@gmail.com>
2023-05-20 15:21:53 +0300

    fix validation

--- src/users/dto/ban-user.dto.ts
+++ src/users/dto/ban-user.dto.ts
@@ -3,6 +3,7 @@ import {IsNumber, IsString} from "class-validator";
 export class BanUserDto{
     @IsNumber({}, {message: 'Только числа'})
     readonly userId: number;
+
     @IsString({message: " Должно быть строкой"})
     readonly banReason: string;
 }
--- src/users/dto/create-VkUserGoogle.dto.ts
+++ src/users/dto/create-VkUserGoogle.dto.ts
@@ -2,22 +2,31 @@
 которые преднозначены для обмена данными между подсистемами
  */
 
-import {IsEmail, IsString, Length} from "class-validator";
+import {IsEmail, IsOptional, IsString, Length} from "class-validator";
 
 export class CreateUserVkGoogleDto {
 
     @IsString({message: " Должно быть строкой"})
     @IsEmail({},{message: 'Некорректный емайл'})
     readonly email: string;
+
     @IsString({message: " Должно быть строкой"})
     @Length(8, 16, {message: "Пароль от 8 до 16 симолов"})
+    @IsOptional()
     readonly password?: string;
+
     @IsString({message: " Должно быть строкой"})
     readonly displayName: string;
+
     @IsString({message: " Должно быть строкой"})
+    @IsOptional()
     verificationToken?: string
+
     @IsString({message: " Должно быть строкой"})
+    @IsOptional()
     provider?: string
+
     @IsString({message: " Должно быть строкой"})
+    @IsOptional()
     userToken?: string
 }
