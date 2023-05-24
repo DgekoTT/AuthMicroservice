f5eb96308f2be0755313db74b5f75d8a787c54b4
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-05-25 00:40:21 +0300

    fix check email

--- src/auth/dto/check-mail.dto.ts
+++ src/auth/dto/check-mail.dto.ts
@@ -5,6 +5,6 @@ export class CheckMailDto {
     @ApiProperty({example: 'user@gmail.com', description: 'почта'})
     @IsString({message: " Должно быть строкой"})
     @IsEmail({},{message: 'Некорректный емайл'})
-    @Matches(/^[a-zA-Z0-9_.+-]+@gmail\.[a-zA-Z0-9_.+-]+$/, { message: 'Некорректный емайл' })
+    @Matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z]+\.[a-zA-Z]+$/, { message: 'Некорректный емайл' })
     readonly email: string
 }
