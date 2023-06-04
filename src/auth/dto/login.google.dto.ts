687103f1652083f1df74f330752bbd8e8abdd0e3
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-06-04 15:00:53 +0300

    fix google

--- src/auth/dto/login.google.dto.ts
+++ src/auth/dto/login.google.dto.ts
@@ -4,8 +4,6 @@ import {IsEmail, IsString, Matches} from "class-validator";
 export class GoogleLoginDto {
     @ApiProperty({example: 'user@gmail.com', description: 'почта'})
     @IsString({message: " Должно быть строкой"})
-    @IsEmail({},{message: 'Некорректный емайл'})
-    @Matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z]+\.[a-zA-Z]+$/, { message: 'Некорректный емайл' })
     readonly email: string
 
     @ApiProperty({example: 'Иван Иваныч', description: 'Имя пользователя или ФИО'})
